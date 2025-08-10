e6d3471eeb9cac8d0c4845d05a12092f248ecf47
oglisa-dev <ognjen.dev@outlook.com>
2025-08-10 14:40:15 +0200

    Update headers in Axios request for simulator route to improve API compliance and request handling, including changes to User-Agent, Accept, and additional security-related headers.

--- src/routes/simulator.route.ts
+++ src/routes/simulator.route.ts
@@ -35,12 +35,19 @@ SimulatorRouter.get("/simulator/ct", async (req: Request, res: Response) => {
             headers: {
                 "Content-Type": "application/json",
                 "User-Agent":
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-                "Accept-Language": "en-US,en;q=0.9",
-                "Accept-Encoding": "gzip, deflate, br",
-                DNT: "1",
-                Connection: "keep-alive",
-                "Upgrade-Insecure-Requests": "1"
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
+                Accept: "*/*",
+                "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+                "Accept-Encoding": "gzip, deflate, br, zstd",
+                Origin: "https://casinoscores.com",
+                Referer: "https://casinoscores.com/",
+                "sec-ch-ua": '"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"',
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": '"macOS"',
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-site",
+                priority: "u=1, i"
             }
         });
 
