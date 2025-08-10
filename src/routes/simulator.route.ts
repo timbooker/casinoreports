88ef89fab4d30d82d5c532e9aebdb67c1851647f
oglisa-dev <ognjen.dev@outlook.com>
2025-08-10 14:38:14 +0200

    Add additional headers to Axios request in simulator route for enhanced API compliance and improved request handling.

--- src/routes/simulator.route.ts
+++ src/routes/simulator.route.ts
@@ -33,7 +33,14 @@ SimulatorRouter.get("/simulator/ct", async (req: Request, res: Response) => {
         const URL = `${CASINO_GAME_SIMULATOR_BASE_URL}/ct`;
         const response = await axios.get(URL, {
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "User-Agent":
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Accept-Encoding": "gzip, deflate, br",
+                DNT: "1",
+                Connection: "keep-alive",
+                "Upgrade-Insecure-Requests": "1"
             }
         });
 
